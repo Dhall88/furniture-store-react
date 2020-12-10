75246837b62fcb9b8f259d350936edc3d26b86ed
David Hall <davidchall388@gmail.com>
2020-12-10 18:40:23 -0500

    storing database respone

--- src/views/home.js
+++ src/views/home.js
@@ -23,9 +23,11 @@ import { ProductContextProvider } from "../context/product-context";
 //     console.log('Fetch Error :-S', err);
 //   });
 
+let test = [];
+
 fetch('http://localhost:3000/products')
   .then(response => response.json())
-  .then(data => console.log(data[0]));
+  .then(data => console.log([...data]));
 
 export default function Home() {
   return (
